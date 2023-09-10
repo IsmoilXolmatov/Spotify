bcb4e0ba401eb840c106de037db80525cc46e1d7
IsmoilXolmatov <ismoilxolmatov69@gmail.com>
2023-09-10 17:13:51 +0500

    first commit

--- Spotify/Controllers/CoreVCs/HomeVC.swift
+++ Spotify/Controllers/CoreVCs/HomeVC.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        title = "Hello World"
+        view.backgroundColor = .orange
+     }
 
 
 }
